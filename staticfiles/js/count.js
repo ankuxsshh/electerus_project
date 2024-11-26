3bdaed7ee7c4e0b2f6d8db505ae6142aba7c5011
ankuxsshh <ankuxshh72@gmail.com>
2024-11-26 13:36:24 +0530

    first commit

--- staticfiles/js/count.js
+++ staticfiles/js/count.js
@@ -1,7 +1,7 @@
 // Function to animate number counting
 function countToNumber(elementId, endValue) {
     let startValue = 0;
-    let duration = 2000; // Duration in milliseconds
+    let duration = 8000; // Duration in milliseconds
     let stepTime = Math.abs(Math.floor(duration / endValue));
     let element = document.getElementById(elementId);
   
