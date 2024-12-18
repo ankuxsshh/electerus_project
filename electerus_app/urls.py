d0516d88b621be206cf7d448731400d5c013f9d0
ankuxsshh <ankuxshh72@gmail.com>
2024-12-18 12:28:21 +0530

    first commit

--- electerus_app/urls.py
+++ electerus_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('projects', views.projects, name='projects'),
     path('services', views.services, name='services'),
